72e51c5d4de825d398ce3cbebbb747f121b81a37
John Vilsack <vilsack@gmail.com>
2025-05-13 16:24:34 -0500

    final final

--- kill-icons.sh
+++ kill-icons.sh
@@ -4,18 +4,22 @@ DOCK_PLIST="$HOME/Library/Preferences/com.apple.dock.plist"
 
 /usr/libexec/PlistBuddy -c "Clear persistent-apps" "$DOCK_PLIST"
 
-function add_app {
-  APP_PATH="$1"
-  /usr/libexec/PlistBuddy -c "Add persistent-apps:0 dict" "$DOCK_PLIST"
-  /usr/libexec/PlistBuddy -c "Add persistent-apps:0:tile-data dict" "$DOCK_PLIST"
-  /usr/libexec/PlistBuddy -c "Add persistent-apps:0:tile-data:file-data dict" "$DOCK_PLIST"
-  /usr/libexec/PlistBuddy -c "Add persistent-apps:0:tile-data:file-data:_CFURLString string file://${APP_PATH}" "$DOCK_PLIST"
-  /usr/libexec/PlistBuddy -c "Add persistent-apps:0:tile-data:file-data:_CFURLStringType integer 15" "$DOCK_PLIST"
-  /usr/libexec/PlistBuddy -c "Add persistent-apps:0:tile-type string file-tile" "$DOCK_PLIST"
-}
+# App list with full paths
+APPS=(
+  "/Applications/Safari.app"
+  "/System/Applications/App Store.app"
+  "/System/Applications/System Settings.app"
+)
 
-add_app "/Applications/Safari.app"
-add_app "/System/Applications/App Store.app"
-add_app "/System/Applications/System Settings.app"
+INDEX=0
+for APP_PATH in "${APPS[@]}"; do
+  /usr/libexec/PlistBuddy -c "Add persistent-apps:$INDEX dict" "$DOCK_PLIST"
+  /usr/libexec/PlistBuddy -c "Add persistent-apps:$INDEX:tile-data dict" "$DOCK_PLIST"
+  /usr/libexec/PlistBuddy -c "Add persistent-apps:$INDEX:tile-data:file-data dict" "$DOCK_PLIST"
+  /usr/libexec/PlistBuddy -c "Add persistent-apps:$INDEX:tile-data:file-data:_CFURLString string file://${APP_PATH}" "$DOCK_PLIST"
+  /usr/libexec/PlistBuddy -c "Add persistent-apps:$INDEX:tile-data:file-data:_CFURLStringType integer 15" "$DOCK_PLIST"
+  /usr/libexec/PlistBuddy -c "Add persistent-apps:$INDEX:tile-type string file-tile" "$DOCK_PLIST"
+  INDEX=$((INDEX + 1))
+done
 
 killall Dock
