505e494de4d9f4cd200aa815722d476a413343b0
John Vilsack <vilsack@gmail.com>
2025-05-14 15:11:09 -0500

    Update kill-icons.sh

--- kill-icons.sh
+++ kill-icons.sh
@@ -4,16 +4,9 @@ echo "Removing all icons from the Dock..."
 defaults delete com.apple.dock persistent-apps
 defaults write com.apple.dock persistent-apps -array
 
-echo "Adding Safari to the Dock..."
-defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Safari.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+# Add Safari to the Dock
+defaults write com.apple.dock persistent-apps -array-add \
+'{"tile-data"={"file-data"={"_CFURLString"="file:///System/Applications/Safari.app";"_CFURLStringType"=15;};};"tile-type"="file-tile";}'
 
-echo "Adding App Store to the Dock..."
-defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/System/Applications/App Store.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-
-echo "Adding System Settings to the Dock..."
-defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/System/Applications/System Settings.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-
-echo "Restarting the Dock to apply changes..."
 killall Dock
 
-echo "Done! Your Dock now has only Safari, App Store, and System Settings."
