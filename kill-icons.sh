2699e9f3df5c1458401d04b2439a014ec6f16c67
John Vilsack <vilsack@gmail.com>
2025-05-14 14:23:45 -0500

    Update kill-icons.sh

--- kill-icons.sh
+++ kill-icons.sh
@@ -3,6 +3,18 @@
 # Remove all persistent apps from the Dock
 defaults write com.apple.dock persistent-apps -array
 
+# Add Safari
+defaults write com.apple.dock persistent-apps -array-add \
+    '{"tile-data" = {"file-data" = {"_CFURLString" = "/System/Applications/Safari.app"; "_CFURLStringType" = 0;};}; "tile-type" = "file-tile";}'
+
+# Add App Store
+defaults write com.apple.dock persistent-apps -array-add \
+    '{"tile-data" = {"file-data" = {"_CFURLString" = "/System/Applications/App Store.app"; "_CFURLStringType" = 0;};}; "tile-type" = "file-tile";}'
+
+# Add System Settings (macOS Ventura and later)
+defaults write com.apple.dock persistent-apps -array-add \
+    '{"tile-data" = {"file-data" = {"_CFURLString" = "/System/Applications/System Settings.app"; "_CFURLStringType" = 0;};}; "tile-type" = "file-tile";}'
+
 # Kill the Dock to apply changes
 killall Dock
 
