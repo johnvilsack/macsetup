a39c85a77f616cd3760e6c5b650bb3378921ed7c
John Vilsack <vilsack@gmail.com>
2025-05-14 14:36:29 -0500

    Update kill-icons.sh

--- kill-icons.sh
+++ kill-icons.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#Current Apps
-defaults read com.apple.dock persistent-apps | grep _CFURLString
-
 # Clear all persistent apps from the Dock
 defaults write com.apple.dock persistent-apps -array
 
@@ -18,11 +15,14 @@ defaults write com.apple.dock persistent-apps -array-add \
 defaults write com.apple.dock persistent-apps -array-add \
     '{"tile-data" = {"file-data" = {"_CFURLString" = "file:///System/Applications/System%20Settings.app"; "_CFURLStringType" = 15;};}; "tile-type" = "file-tile";}'
 
-defaults read com.apple.dock persistent-apps
+#Current Apps
+echo '###'
+defaults read com.apple.dock persistent-apps | grep _CFURLString
+echo '###'
 
 # Apply changes
 killall Dock
 
-defaults read com.apple.dock persistent-apps | grep _CFURLString
+
 
 echo "Dock icons updated."
