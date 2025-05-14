de45cbbc41e3f972095d8934991cbe14f2df58ec
John Vilsack <vilsack@gmail.com>
2025-05-14 15:01:59 -0500

    Update kill-icons.sh

--- kill-icons.sh
+++ kill-icons.sh
@@ -8,3 +8,4 @@ defaults write com.apple.dock persistent-apps -array
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/System/Applications/Stocks.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 
 killall Dock
+echo "Done"
