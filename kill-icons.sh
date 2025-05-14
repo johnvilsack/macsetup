d1dc17ac0194c882ca4382b2fde2fd3db8d2ed52
John Vilsack <vilsack@gmail.com>
2025-05-14 14:52:41 -0500

    Update kill-icons.sh

--- kill-icons.sh
+++ kill-icons.sh
@@ -1,19 +1,15 @@
-
 #!/bin/bash
 
-echo "Removing all icons from the Dock..."
+# Remove all persistent apps from the Dock
 defaults write com.apple.dock persistent-apps -array
 
-echo "Adding Safari to the Dock..."
-defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="file-tile";"tile-data"={"file-label"="Safari";"bundle-identifier"="com.apple.Safari";"file-data"={"_CFURLString"="/Applications/Safari.app";"_CFURLStringType"=0;};};}' 
+# Add desired apps (Safari, App Store, System Settings)
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Safari.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/App Store.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/System/Applications/System Settings.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 
-echo "Adding App Store to the Dock..."
-defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="file-tile";"tile-data"={"file-label"="App Store";"bundle-identifier"="com.apple.AppStore";"file-data"={"_CFURLString"="/System/Applications/App Store.app";"_CFURLStringType"=0;};};}' 
-
-echo "Adding System Settings to the Dock..."
-defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="file-tile";"tile-data"={"file-label"="System Settings";"bundle-identifier"="com.apple.systempreferences";"file-data"={"_CFURLString"="/System/Applications/System Settings.app";"_CFURLStringType"=0;};};}' 
-
-echo "Restarting the Dock to apply changes..."
+# Restart Dock to apply changes
 killall Dock
 
-echo "Done! Your Dock now has only Safari, App Store, and System Settings."
+# Notification
+osascript -e 'display notification "Dock updated with Safari, App Store, and System Settings" with title "Dock Configured"'
